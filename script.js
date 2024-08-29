4bd578dbb1b8f6ed813b549f916d7814249c9f9d
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-29 18:01:23 +0600

    new commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// arithmethic operation
+let a = 9;
+let b = 19;
+
+
+
+let c = a + b;
+// console.log(c);
+// console.log(" a + b =", a + b);
+console.log(a % b);
+// console.log(a, b);
+
+// unary operator
+
+
+
