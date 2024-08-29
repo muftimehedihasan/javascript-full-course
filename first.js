7c4a2b0b6457b98c678e61d3ae2aade8efd6c479
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-29 16:47:25 +0600

    new commit

--- first.js
+++ first.js
@@ -1,2 +1,37 @@
-const PI = 3.14
-console.log(PI);
+
+    // let a = 5;
+    // console.log(a);
+    // typeof a;
+
+
+// const student = {
+//     name: "Mehedi Hasan",
+//     age: 33,
+//     number: 33.333
+// }
+
+// student["age"] = student["age"] + 2 ;
+// console.log(student["age"]);
+// console.log(student.age);
+
+// const product ={
+//     title: "Apple",
+//     offer: 7,
+//     price: 100,
+//     reting: 4
+// };
+
+// console.log(product);
+
+
+const profile = {
+    name: "Sradha Khapra",
+    follow: true,
+    follwer: 100,
+    designation: "ex-microsoft",
+};
+
+// profile["follwer"] = profile["follwer"] + 3;
+// console.log(profile["follwer"]);
+
+console.log(profile);
