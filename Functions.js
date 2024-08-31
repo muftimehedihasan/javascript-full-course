9c45e0930f345ee19b92a6ec4297ad96f3ab4145
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-31 17:11:45 +0600

    new commit

--- Functions.js
+++ Functions.js
@@ -5,10 +5,54 @@
 // }
 // message("Hello Brothres"); // argument
 
-function hello(msg, n){
-    // parameter- input
-    console.log(msg, n);
-}
+// function hello(msg, n){
+//     // parameter- input
+//     console.log(msg, n);
+// }
+// hello("Hello Msgs", 1000);
 
-hello("Hello Msgs", 1000);
+// function sum(x, y){
+//    console.log(s = x + y);
+//    return s; 
+// }
+
+// sum(10, 12);
+
+// function sum(x, y){
+//     sumation = x + y
+//     return sumation;
+// }
+
+// let result = sum(12, 8);
+// console.log(result);
+
+
+// Arrow function
+
+// const arrowSum = (a, b) =>{
+//     console.log(a + b);
+// }
+
+// arrowSum(2, 5);
+// // console.log(arrowSum);
+
+// multiplication function
+
+// const multiplication = (a, b) => {
+//     mul = a * b
+//     console.log(mul);
+//     return mul;
+// }
+
+// multiplication(34, 9);
+
+// let printHello = () =>{
+//     console.log("Hello")
+// }
+
+// printHello();
+
+let printHello = () => console.log("Hello");
+
+printHello();
 
