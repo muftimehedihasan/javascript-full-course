b062ba1b5d1764507196d4afec5f8317789e03c3
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-31 12:59:36 +0600

    new commit

---
+++ Functions.js
@@ -0,0 +1,14 @@
+
+// function message(msg){
+//     // peramiter -> input
+//     console.log(msg);
+// }
+// message("Hello Brothres"); // argument
+
+function hello(msg, n){
+    // parameter- input
+    console.log(msg, n);
+}
+
+hello("Hello Msgs", 1000);
+
---
+++ practice8.js
@@ -0,0 +1,8 @@
+companies = ["Bloomerg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+
+// companies.shift();
+// companies.splice(2, 1, "Ola");
+// companies.unshift("Zoho");
+companies.push("Amazon");
+console.log(companies);
